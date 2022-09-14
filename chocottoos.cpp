940482c47fb62b1241db967d7b3ce8a277067a48
yud0uhu <denham@Yus-MacBook-Pro.local>
2022-09-15 07:04:24 +0900

    fix: シリアル通信依存の不具合を解消

--- chocottoos.cpp
+++ chocottoos.cpp
@@ -10,19 +10,30 @@
 #define TASK_ID_MAX 10
 #define TASK_PRIORITY_NUMBER 10
 
-unsigned char run_queue;
-typedef unsigned int Priority;
+typedef volatile uint8_t Priority;
 
-void create_task(unsigned char task_id, Priority priority, void (*task)(void));
+void all_set_task(struct task_define *tasks);
+void create_task(volatile unsigned char task_id, Priority priority, void (*task)(void));
+void context_switch(void);
 unsigned char get_top_ready_id(void);
 void os_start(void);
-struct TCB *readyqueue_deque(int priority);
+struct TCB *readyqueue_deque(Priority priority);
 void readyqueue_enque(struct TCB *p_tcb_);
 void schedule(void);
 void timer_create(void);
-void task_start(unsigned char current_task_id);
+void task_reload(void);
+void task_start(void);
 void timer_init(void);
-int timer_handler(void);
+void init_all(void);
+
+struct task_define
+{
+    volatile unsigned char task_id_;
+    Priority priority_;
+    void (*task_handler_)(void);
+};
+struct task_define tasks[] = {};
+struct task_define load_tasks[] = {};
 
 enum TaskState
 {
@@ -31,32 +42,28 @@ enum TaskState
     SUSPEND
 };
 
-/* task control block */
 typedef struct TCB
 {
-    unsigned char task_id_; /* タスクの識別子 0~255の整数値*/
-    TaskState state_;       /* running, ready, suspendの3つの状態を持つ */
-    Priority priority_;     /* タスクの優先度(0~9) */
-    struct TCB *p_prev_;    /* 前のタスク p_next_*/
-    struct TCB *p_next_;    /* 次のタスク p_prev_*/
+    volatile unsigned char task_id_;
+    TaskState state_;
+    Priority priority_;
+    struct TCB *p_prev_;
+    struct TCB *p_next_;
     void (*task_handler_)(void);
 } tcb_t;
-tcb_t TCB[3];
 
-// 実行可能状態のタスクを登録するリスト
+tcb_t TCB[TASK_ID_MAX];
+
 tcb_t ready_que[TASK_PRIORITY_NUMBER];
 tcb_t *current;
-tcb_t *load_tcb;
 
-struct TCB *readyqueue_deque(int priority)
+struct TCB *readyqueue_deque(Priority priority)
 {
     struct TCB *p_ready_que_ = &ready_que[priority];
 
     struct TCB *p_tcb_;
 
     p_tcb_ = p_ready_que_->p_next_;
-
-    task_reload();
 
     if (p_tcb_)
     {
@@ -73,7 +80,7 @@ struct TCB *readyqueue_deque(int priority)
 
 void readyqueue_enque(struct TCB *p_tcb_)
 {
-    struct TCB *p_ready_que_ = &ready_que[p_tcb_->priority_];
+    tcb_t *p_ready_que_ = &ready_que[p_tcb_->priority_];
 
     p_tcb_->p_next_ = 0;
 
@@ -90,53 +97,50 @@ void readyqueue_enque(struct TCB *p_tcb_)
 
 void schedule(void)
 {
-    // タスクの初期化
-    int i;
+    uint8_t i;
     for (i = 0; i < TASK_ID_MAX; i++)
     {
         current = readyqueue_deque(i);
         if (current)
+        {
             return;
+        }
     }
 }
 
-void create_task(unsigned char task_id, Priority priority, void (*task)(void))
+void create_task(volatile unsigned char task_id, Priority priority, void (*task)(void))
 {
-
     if (task_id >= TASK_ID_MAX)
         exit(1);
-    if (&TCB[task_id] == 0)
-    {
-        os_start();
-    }
 
     current = &TCB[task_id];
-    load_tcb = current;
-
     current->task_id_ = task_id;
     current->priority_ = priority;
     current->task_handler_ = task;
     current->state_ = READY;
 
     readyqueue_enque(current);
+
+    return;
 }
 
 void context_switch(void)
 {
-    // タスクのコンテキストを保存
     if (current->state_ == READY)
+    {
         readyqueue_enque(current);
-
+        current->state_ = RUNNING;
+    }
+    else
+        task_reload();
     schedule();
 }
 
 unsigned char get_top_ready_id(void)
 {
-    struct TCB *p_tcb_;
     Priority priority_;
     for (priority_ = 0; priority_ < TASK_PRIORITY_NUMBER; priority_++)
     {
-        //  優先順位順に連結されているTCBリストの末尾まで探索し
         if (ready_que[priority_].p_next_ != 0)
         {
             return ready_que[priority_].p_next_->task_id_;
@@ -145,42 +149,26 @@ unsigned char get_top_ready_id(void)
     return 0;
 }
 
-unsigned char current_task_id;
 void os_start(void)
+
 {
-    // シリアル通信の遅延が処理に作用しているため、printはそのままにする
-    Serial.println("OS_START");
-    current_task_id = get_top_ready_id();
-
+    if (get_top_ready_id() == 0)
+    {
+        task_reload();
+    }
+    current->task_id_ = get_top_ready_id();
+    current->state_ = READY;
     schedule();
-    current[current_task_id] = TCB[current_task_id];
-    current[current_task_id].state_ = RUNNING;
-
     context_switch();
-    task_start(current_task_id);
-
+    task_start();
     sei();
 }
 
-void task_reload()
+void task_start()
 {
-    if (current_task_id == 0)
+    if (current->task_id_ != 0)
     {
-        cli();
-        // シリアル通信の遅延が処理に作用しているため、printはそのままにする
-        Serial.println("TASK_RELOAD");
-        current = load_tcb;
-        current_task_id = current->task_id_;
-        current_task_id = current_task_id + 1;
-        return;
-    }
-}
-
-void task_start(unsigned char current_id)
-{
-    if (current[current_task_id].task_id_ != 0)
-    {
-        (*current[current_task_id].task_handler_)();
+        (*current->task_handler_)();
     }
 }
 
@@ -192,8 +180,8 @@ void timer_init(void)
 void timer_create(void)
 {
     cli();
-    TCCR1A = 0;
-    TCCR1B = 0;
+    TCCR1A = 0x00;
+    TCCR1B = 0x00;
     TCNT1 = 3036;
     TCCR1B |= _BV(CS12);  // 256分周, CTCモード
     TIMSK1 |= _BV(TOIE1); // オーバーフロー割り込みを許可
@@ -203,6 +191,18 @@ ISR(TIMER1_OVF_vect)
 {
     os_start();
 }
+
+void init_all(void)
+{
+    memset(TCB, 0, sizeof(TCB));
+    memset(current, 0, sizeof(current));
+}
+
+void task_reload()
+{
+    all_set_task(load_tasks);
+}
+
 void LED1(void)
 {
     digitalWrite(2, !digitalRead(2));
@@ -218,20 +218,32 @@ void LED3(void)
     digitalWrite(4, !digitalRead(4));
 }
 
+void all_set_task(struct task_define *tasks)
+{
+    for (int i = 0; i < sizeof(tasks) + 1; i++)
+    {
+        memcpy(&load_tasks[i], &tasks[i], sizeof(tasks[i]));
+        create_task(tasks[i].task_id_, tasks[i].priority_, tasks[i].task_handler_);
+    }
+}
 void setup()
 {
-    Serial.begin(9600);
 
-    memset(TCB, 0, sizeof(TCB));
-    memset(ready_que, 0, sizeof(ready_que));
+    Serial.begin(115200);
+    init_all();
 
     pinMode(2, OUTPUT);
     pinMode(3, OUTPUT);
     pinMode(4, OUTPUT);
 
-    create_task(1, 6, LED1);
-    create_task(2, 4, LED2);
-    create_task(3, 5, LED3);
+    struct task_define tasks[] = {
+        {2, 2, LED1},
+        {3, 3, LED2},
+        {4, 4, LED3},
+    };
+
+    all_set_task(tasks);
+
     timer_create();
     os_start();
 
